30f135882bf2c3b38d8671fa561fda4ef893f1d3
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-05-15 10:26:22 +0800

    done basics

--- React/scbrd.jsx
+++ React/scbrd.jsx
@@ -1,5 +1,28 @@
 // import 'index.css';
 
+let aPlayers = [
+    {
+        id: '1',
+        name: 'una',
+        score: 12
+    },
+    {
+        id: '2',
+        name: 'lawa',
+        score: 16
+    },
+    {
+        id: 3,
+        name: 'san',
+        score: 97
+    },
+    {
+        id: 4,
+        name: 'pat',
+        score: 23
+    },
+];
+
 const Header = (p) => 
 <header id="head" className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <h1>{p.title}</h1>
@@ -7,40 +30,47 @@ const Header = (p) =>
 </header>
 ;
 
-const Player = () => {
+const Player = (p) => {
     return (
         <div className="player">
             <span className="playerName">
-                Scei
+                {p.pName}
             </span>
-            <Counter />
+            <Counter score={p.pScore}/>
         </div>
     );
 }
 
-const Counter = () =>{
+const Counter = (p) =>{
     return(
         <div className="counter">
             <button className="count-act decrease">-</button>
-            <span className="count score">69</span>
+            <span className="count score">{ p.score }</span>
             <button className="count-act increase">+</button>
         </div>
     );
 }
 
-const App = () =>{
+const App = (p) =>{
     return(
         <div className="counter">
-            <Header title='Scoreboard' tPlayers='7'/>
-
+            <Header title='Scoreboard' tPlayers={p.allPlayers.length}/>
             {/* players list */}
-            <Player /> 
+            {
+                p.allPlayers.map( player =>
+                <Player 
+                pName={player.name}
+                pScore={player.score}
+                key={player.id.toString()}
+                /> 
+                 )
+            }
         </div>
     );
 }
 
 ReactDOM.render(
-    <App />,
+    <App allPlayers={aPlayers}/>,
     document.getElementById('root')
 );
 
