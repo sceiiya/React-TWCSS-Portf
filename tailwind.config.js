f8b414c5603a1b3d28798b183edab7330c6eafea
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-16 17:11:14 +0800

    reorganize directories

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     ".jsx",
+    './index.html', './src/**/*.{js,ts,jsx,tsx}', '*.html', '*.php', '*.jsx'
   ],
   theme: {
     extend: {},
