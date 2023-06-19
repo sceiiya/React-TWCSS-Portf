bf83a6e9b1fed60250c68884daab6c335b1eea7a
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-19 23:35:20 +0800

    improve body

--- src/digital-business-card/components/AboutMe.jsx
+++ src/digital-business-card/components/AboutMe.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const AboutMe = () => {
     return(
-        <>
+        <span>
             <h3>About Me</h3>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum. Sociis natoque penatibus et magnis dis.
             </p>
-        </>
+        </span>
     )
 }
 
--- src/digital-business-card/components/Interests.jsx
+++ src/digital-business-card/components/Interests.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Interests = () => {
     return(
-        <>
+        <span>
             <h3>Interest</h3>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum. Sociis natoque penatibus et magnis dis.
             </p>
-        </>
+        </span>
     )
 }
 
