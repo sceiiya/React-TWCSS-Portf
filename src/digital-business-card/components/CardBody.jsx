887974c1710c288394dd0963f67f79483d2abb5f
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-19 23:34:20 +0800

    reorganize card body

---
+++ src/digital-business-card/components/CardBody.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutMe from "./AboutMe";
+import Interests from "./Interests";
+
+const CardBody = () => {
+    <span>
+        <AboutMe />
+        <Interests />
+    </span>
+}
+
+export default CardBody;
