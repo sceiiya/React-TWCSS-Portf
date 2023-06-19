4fb461c1a58952a0cc26510795e2647215f28fc1
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-19 23:34:36 +0800

    reorganize card header

---
+++ src/digital-business-card/components/CardHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Profile from "./Profile";
+import Name from "./Name";
+import Profession from "./Profession";
+import ContactWith from "./ContactWith";
+
+const CardHeader = () => {
+    <span>
+        <Profile />
+        <Name />
+        <Profession />
+        <ContactWith />
+    </span>
+}
+
+export default CardHeader;
