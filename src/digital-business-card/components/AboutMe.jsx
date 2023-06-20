2d58ba5f64edd66e7c036154fa040b812c231cde
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-21 00:29:06 +0800

    add padding to body component

--- src/digital-business-card/components/AboutMe.jsx
+++ src/digital-business-card/components/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutMe = () => {
     return(
-        <span>
+        <span className='p-2'>
             <h3>About Me</h3>
             <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. At augue eget arcu dictum. Sociis natoque penatibus et magnis dis.
