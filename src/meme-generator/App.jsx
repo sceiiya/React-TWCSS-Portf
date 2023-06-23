54f821cf21b4a0c9aac01b492b86f9abb654ed40
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-23 22:02:42 +0800

    fix input

--- src/meme-generator/App.jsx
+++ src/meme-generator/App.jsx
@@ -40,8 +40,8 @@ const App = () => {
         <main className='p-6'>
             <div className='grid grid-rows-2 gap-4'>
                 <span className='flex flex-row justify-evenly gap-3'>
-                    <input type="text" name="" id="" placeholder='nom' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
-                    <input type="text" name="" id="" placeholder='nom' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+                    <input type="text" placeholder='Above Text' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+                    <input type="text" placeholder='Bottom Text' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
                 </span>
                 <button onClick={handleNewMemeImage} className='mx-auto w-full bg-emerald-300 font-bold text-lg text-white p-2 rounded-md'>Generate Random Meme Image</button>
                 {/* type='submit' */}
