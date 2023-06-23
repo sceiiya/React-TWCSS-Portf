d2335b1210cd5d61642bd2071a9aba948636cd13
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-23 20:11:37 +0800

    new project meme generator

---
+++ src/meme-generator/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class App extends Component {
+
+    //two types of state
+    //Application State //resusable for its children components
+    //Component State //local component state
+    render(){
+        return(
+            <h1>
+                asdwekaje
+            </h1>
+        )
+    }
+}
+
+export default App;
