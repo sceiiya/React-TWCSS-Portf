f703ac410af33e37c5f14d1a96ad50ae9eb947b1
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-24 20:14:03 +0800

    add state

--- src/meme-generator/App.jsx
+++ src/meme-generator/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import sample from '../../src/digital-business-card/assets/images/profile.jpg'
 import memesData from './context/memesData';
@@ -7,24 +7,35 @@ import memesData from './context/memesData';
     //Component State //local component state
 
 const App = () => {
-    function handleTest(){
-        alert('test');
-    }
+    const memeData = {
+        topText: '',
+        bottomText: '',
+        randomImage: 'http://i.imgflip.com/1bij.jpg' 
+    };
 
-    const memesArr = memesData.data.memes;
-    
+    const memesRawArr = memesData.data.memes;
+    const [memesArr, setMemesArr] = useState(memesRawArr);
+    const [MemeData, setMemeData] = useState(memeData);
+    const {topText, bottomText, randomImage} = MemeData;
+    // console.log(MemeData)
+
     const rn = () => {
         return Math.floor(Math.random()*memesArr.length);
     }
 
     const newMemeImg = () => {
+
         const {url} = memesArr[rn()];
         // return memesArr[rn()].url;
         return url;
     }
 
     const handleNewMemeImage = () => {
-        console.log(newMemeImg());
+        setMemeData( prevMemeData => ({
+            ...prevMemeData,
+            'randomImage' : newMemeImg()
+        })  )
+        // console.log(newMemeImg());
     }
     
     return(
@@ -37,17 +48,17 @@ const App = () => {
             </span>
         </header>      
         
-        <main className='p-6'>
+        <main className='p-6 px-48 max-md:px-24 max-sm:px-8'>
             <div className='grid grid-rows-2 gap-4'>
                 <span className='flex flex-row justify-evenly gap-3'>
-                    <input type="text" placeholder='Above Text' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
+                    <input type="text" placeholder='Top Text' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
                     <input type="text" placeholder='Bottom Text' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
                 </span>
                 <button onClick={handleNewMemeImage} className='mx-auto w-full bg-emerald-300 font-bold text-lg text-white p-2 rounded-md'>Generate Random Meme Image</button>
                 {/* type='submit' */}
             </div>
             <div className='w-full'>
-                <img src={sample} alt="" />
+                <img src={randomImage} alt="" className='content-center w-full max-h-96 object-contain bg-slate-100'/>
             </div>
         </main>
         </>
