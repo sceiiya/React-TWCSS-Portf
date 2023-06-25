0338123c5003cacef34e0167b40eaffad0ad286b
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-25 10:35:49 +0800

    Update ternary_operator.js error on vscode git remote fatal

--- src/meme-generator/context/ternary_operator.js
+++ src/meme-generator/context/ternary_operator.js
@@ -19,3 +19,16 @@ console.log(answer3); // Output: Yes
 isGoingOut = false;
 answer3 = isGoingOut ? "Yes" : "No";
 console.log(answer3); // Output: No
+
+
+// these is called conditional rendering
+// the two below is the same
+{isShown ? <p>{props.punchline}</p> : ''}
+{isShown && <p>{props.punchline}</p>}
+{messages.length > 1 && "s"}
+{messages.length > 1 ? "messages" : "message"}
+
+//note
+// ampersant method is great if you want to display or not display
+
+// ternary method is great if you want to choose between two entity to diplay
