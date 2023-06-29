5c12614e56ee43822bbdbda25c02fb6fd0ebb5d5
sceiiya <124661065+sceiiya@users.noreply.github.com>
2023-06-29 18:10:56 +0800

    add maxlength to input

--- src/meme-generator/App.jsx
+++ src/meme-generator/App.jsx
@@ -62,10 +62,12 @@ const App = () => {
                 <span className='flex flex-row justify-evenly gap-3'>
                     <input type="text" placeholder='Top Text'
                     name='topText'
+                    maxLength='35'
                     onChange={handleChange}
                     className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
                     <input type="text" placeholder='Bottom Text'
                     name='bottomText'
+                    maxLength='35'
                     onChange={handleChange}
                     className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'/>
                 </span>
